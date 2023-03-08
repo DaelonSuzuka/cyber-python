50c7a6f17c532002ace33e0b9cd6d419747dce25
David Kincaid <daelonsuzuka@gmail.com>
2023-03-08 15:29:23 -0500

    Add tests for the validate function

---
+++ test/test_validate.py
@@ -0,0 +1,20 @@
+from cyber import CyberVM, CyResultCode, CyType
+
+
+def test_validate_pass():
+    cyber = CyberVM()
+    cyber.validate('none')
+
+    assert cyber.last_result == CyResultCode.CY_Success
+
+# def test_validate_token_error():
+#     cyber = CyberVM()
+#     cyber.validate('')
+
+#     assert cyber.last_result == CyResultCode.CY_ErrorCompile
+
+def test_validate_compile_error():
+    cyber = CyberVM()
+    cyber.validate('null')
+
+    assert cyber.last_result == CyResultCode.CY_ErrorCompile
