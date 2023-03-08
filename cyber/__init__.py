6e17cfd21de47bb07da8a6351037ad62e4b97d18
David Kincaid <daelonsuzuka@gmail.com>
2023-03-08 15:28:20 -0500

    Add jupyter notebook support

--- cyber/__init__.py
+++ cyber/__init__.py
@@ -1,1 +1,28 @@
 from .vm import *
+
+
+try:
+    from IPython.core.magic import Magics, magics_class, line_cell_magic
+
+    @magics_class
+    class CyberMagics(Magics):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self._cyber = CyberVM()
+            
+            @self._cyber.function('core.print')
+            def _print(string: str):
+                print(string)
+
+        @line_cell_magic
+        def cyber(self, line, cell=None):
+            if cell is None:
+                self._cyber.eval(line)
+            else:
+                self._cyber.eval(cell)
+
+    def load_ipython_extension(ipython):
+        ipython.register_magics(CyberMagics)
+
+except ImportError:
+    pass
